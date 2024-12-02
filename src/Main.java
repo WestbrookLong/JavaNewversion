4bc9153da53c5e652428e31a3529bdb9a87308fd
csjjjjjjj <12412625@mail.sustech.edu.cn>
2024-12-02 16:05:25 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,35 @@
-import view.level.LevelFrame;
-import view.login.LoginFrame;
+import view.level.LevelFrame; 
+import view.login.LoginFrame; 
 
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            LoginFrame loginFrame = new LoginFrame(280,280);
-            loginFrame.setVisible(true);
-            LevelFrame levelFrame = new LevelFrame(500,400);
-            levelFrame.setVisible(false);
+          
+            LoginFrame loginFrame = new LoginFrame(280, 280);
+            LevelFrame levelFrame = new LevelFrame(500, 200);
             loginFrame.setLevelFrame(levelFrame);
+
+           
+            String[] options = {"游客模式", "用户模式"};
+            int choice = JOptionPane.showOptionDialog(null,
+                    "请选择游戏模式",
+                    "模式选择",
+                    JOptionPane.DEFAULT_OPTION,
+                    
+                    JOptionPane.INFORMATION_MESSAGE,
+                    
+                    null, options, options[0]);
+
+            if (choice == 0) {
+               
+                levelFrame.setVisible(true);
+            } else if (choice == 1) {
+               
+                loginFrame.setVisible(true);
+            }
         });
     }
 }
+
